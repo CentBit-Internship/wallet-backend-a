bd2ac3262d3530b2e1792f90f0b779255c9e28f3
stevechude <stevechude@gmail.com>
2022-07-30 22:59:09 +0100

    update postRequest

--- dist/controllers/ussd_controller.js
+++ dist/controllers/ussd_controller.js
@@ -10,39 +10,39 @@ exports.getRequest = getRequest;
 const postRequest = (req, res) => {
     // console.log(req.body)
     let { sessionId, serviceCode, phoneNumber, text } = req.body;
-    let response;
+    let response = '';
     if (text === "") {
         response = `CON Enter your fullname`;
         res.send(response);
     }
-    else if (text === "") {
+    else if (text !== "") {
         response = `CON What do you want to check
       1. My Account
       2. My Phone Number`;
         res.send(response);
     }
-    else if (text !== "1") {
+    else if (text === "1") {
         response = `CON Choose account information you want to view
         1. Account Name
         2. Account Number
         3. Account Balance`;
         res.send(response);
     }
-    else if (text !== "2") {
+    else if (text === "2") {
         response = `END Your phone number is ${phoneNumber}`;
         res.send(response);
     }
-    else if (text !== "1*1") {
+    else if (text === "1*1") {
         let accountName = "Steve Chude";
         response = `END Your account name is ${accountName}`;
         res.send(response);
     }
-    else if (text !== "1*2") {
+    else if (text === "1*2") {
         let accountNumber = generateAccountNumber();
         response = `END Your account number is ${accountNumber}`;
         res.send(response);
     }
-    else if (text !== "1*3") {
+    else if (text === "1*3") {
         let balance = "NGN 10,000";
         response = `END Your account balance is ${balance}`;
         res.send(response);
