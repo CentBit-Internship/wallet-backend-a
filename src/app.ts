84cb7e923975ea48e2249240deca50ca29d0f70b
stevechude <stevechude@gmail.com>
2022-08-02 02:11:39 +0100

    main

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,31 @@
-import express from 'express';
-import logger from 'morgan';
-import ussdRoute from './routes/router';
-import connectDB from './db_config/db';
-
+import express from "express";
+import logger from "morgan";
+import ussdRoute from "./routes/router";
+import connectDB from "./db_config/db";
+import swaggerDoc from "swagger-ui-express";
+import swaggerDocumentation from "./utils/docs";
 
 require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 // app.use(express.urlencoded({extended: true}));
 
-// Routes
-app.use('/', ussdRoute)
+app.use("/docs", swaggerDoc.serve);
 
-const port = process.env.PORT || 3004
+app.use("/docs", swaggerDoc.setup(swaggerDocumentation));
+
+// Routes
+app.use("/", ussdRoute);
+
+const port = process.env.PORT || 3004;
 
 // DB Connection
 const db_url = `mongodb+srv://stevechude:houseofgrace%401@rest-api.kpvocbv.mongodb.net/?retryWrites=true&w=majority`;
 connectDB(db_url);
 
-app.listen(port, () => console.log(`app running on port ${port}`));
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
